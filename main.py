0bd98b2651b041a39337f00429280175ba6bfc0e
Luthfi Afriansyah <105446118+luthpai@users.noreply.github.com>
2023-11-15 20:20:49 +0700

    change

--- main.py
+++ main.py
@@ -15,19 +15,40 @@ info = """
    License : MIT License
 """
 
-def clear():
-    if name == "nt":
-        system("cls")
-    else:
-        system("clear")
-
-
-def ls():
-    if name == "nt":
-        system("dir")
-    else:
-        system("ls")
-
+class CMD:
+	def clear():
+		if name == "nt":
+			system("cls")
+		else:
+			system("clear")
+	
+	def ls():
+		if name == "nt":
+			system("dir")
+		else:
+			system("ls")
+			
+	def cd(dir):
+		system(f"cd {dir}")
+		
+	def fetch(url):
+		try:
+		    res = req.get(url)
+		    res = res.json()
+		    print(res)
+		except:
+		        print(
+                f"{yellow}Error while doing http request. Please check your internet connection or the url you typed. The url might be invalid or the referenced url is not available or it does not provide any response{white}"
+            )
+            
+	def sysinfo():
+	   print("Operating System: ", platform.system())
+	   print("Platform: ", platform.platform())
+	   print("Machine: ", platform.machine())
+	   print("Node Name: ", platform.node())
+	   print("Python: ", platform.python_version())
+			
+cmd = CMD
 
 cyan = Fore.CYAN
 green = Fore.GREEN
@@ -45,56 +66,48 @@ available = [
     {"name": "ascii", "desc": "transform normal text to ascii"},
     {"name": "sysinfo", "desc": "print out user's device information"},
     {"name": "api", "desc": "simply get the json response from an api"},
+    {"name": "cd", "desc": "change directory"}
 ]
 
 while True:
-    cmd = input(f"{cyan}{user}{host} {green}${reset} ")
-    cmd = cmd.split(" ", 1)
-    if cmd[0] == "help":
-        if len(cmd) == 1:
+    inp = input(f"{cyan}{user}{host} {green}${reset} ")
+    inp = inp.split(" ", 1)
+    if inp[0] == "help":
+        if len(inp) == 1:
             print("Available commands: ")
             for item in available:
                 print(f"{white}{item['name']}{reset} -> {item['desc']}")
             print("\nType \"help -i\" to see the information of this app")
-        elif len(cmd) == 2 and cmd[1] == "-i":
+        elif len(inp) == 2 and inp[1] == "-i":
         	print(info)
-    elif cmd[0] == "clear":
-        clear()
-    elif cmd[0] == "echo":
-        if len(cmd) == 2:
-            print(cmd[1])
+    elif inp[0] == "clear":
+        cmd.clear()
+    elif inp[0] == "echo":
+        if len(inp) == 2:
+            print(inp[1])
         else:
             print(f"{yellow}Please input the parameter.{reset} e.g : echo hello")
-    elif cmd[0] == "ls":
-        ls()
-    elif cmd[0] == "pwd":
+    elif inp[0] == "ls":
+        cmd.ls()
+    elif inp[0] == "pwd":
         system("pwd")
-    elif cmd[0] == "ascii":
-        if len(cmd) == 2:
-            print(text2art(cmd[1]))
+    elif inp[0] == "ascii":
+        if len(inp) == 2:
+            print(text2art(inp[1]))
         else:
             print(f"{yellow}Please input the parameter.{reset} e.g : ascii hello")
-    elif cmd[0] == "sysinfo":
-        print("Operating System: ", platform.system())
-        print("Platform: ", platform.platform())
-        print("Machine: ", platform.machine())
-        print("Node Name: ", platform.node())
-        print("Python: ", platform.python_version())
-    elif cmd[0] == "api":
-        if len(cmd) == 2:
-            try:
-                res = req.get(cmd[1])
-                res = res.json()
-                print(res)
-            except:
-                print(
-                    f"{yellow}Error when doing http request. Please check your internet connection or the url you typed. The url may be invalid or the referenced url is not available or it does not provide any response{white}"
-                )
-                continue
+    elif inp[0] == "sysinfo":
+        cmd.sysinfo()
+    elif inp[0] == "api":
+        if len(inp) == 2:
+            cmd.fetch(inp[1])
+            continue
         else:
             print(f"{yellow}Please input the url.{reset} e.g : api https://example.com")
+    elif inp[0] == "cd":
+    	if len(inp) == 2:
+    		cmd.cd(inp[1])
+    	else:
+    		print(f"{yellow}Invalid. {white}Please input the directory you want to go to.")
     else:
         print(f'{yellow}Command not found.{reset} Run "help" to see listed commands')
-        
-        
-# 100 hundred lines :-)
